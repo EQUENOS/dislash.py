c0b8cda67d32487f558a8c31d6b9d104afd8dc2b
EQUENOS <50338932+EQUENOS@users.noreply.github.com>
2021-02-27 22:52:04 +0300

    Gets version from dislash/__init__.py

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup
 import pathlib
 
+
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 requirements = []
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+with open('dislash/__init__.py', 'r') as f:
+    version = [line.split('=')[1].strip(" '\"") for line in f.read().splitlines() if line.startswith('__version__')][0]
 
 
 setup(
     name='dislash.py',
-    version='0.1.0',
-    description='A python wrapper for discord slash-commands',
+    version=version,
+    description='A python wrapper for discord slash commands.',
     
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/EQUENOS/dislash.py',
     author='EQUENOS',
     author_email='equenos1@gmail.com',
-    keywords='discord, discord-api, slash-commands',
+    keywords='python, discord, slash, commands, api, discord-api, discord-py, slash-commands',
     packages=['dislash'],
     python_requires='>=3.6, <4',
     install_requires=requirements,
