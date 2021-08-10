218f47a76c9d755d56b179ffe7d74ebc148adc37
MisileLab <misileminecord@gmail.com>
2021-08-10 13:18:44 +0900

    style: :art: missing whitespace around parameter equals

--- dislash/interactions/message_components.py
+++ dislash/interactions/message_components.py
@@ -134,7 +134,7 @@ class SelectOption:
 
     __slots__ = ("label", "value", "description", "emoji", "default")
 
-    def __init__(self, label: str, value: str, description: str=None, emoji: str=None, default: bool=False):
+    def __init__(self, label: str, value: str, description: str = None, emoji: str = None, default: bool = False):
         if isinstance(emoji, str):
             emoji = _partial_emoji_converter(emoji)
         
@@ -223,7 +223,7 @@ class SelectMenu(Component):
         the list of chosen options, max 25
     """
 
-    def __init__(self, *, custom_id: str=None, placeholder: str=None, min_values: int=1, max_values: int=1, options: list=None, disabled: bool=False):
+    def __init__(self, *, custom_id: str = None, placeholder: str = None, min_values: int = 1, max_values: int = 1, options: list = None, disabled: bool = False):
         super().__init__(3)
         self.custom_id = custom_id or "0"
         self.placeholder = placeholder
