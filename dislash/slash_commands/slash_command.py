e14891ff4e2630c9fac6351c93d15414810aaeae
MisileLab <misileminecord@gmail.com>
2021-08-10 13:54:27 +0900

    style: :art: multiple spaces before operator

--- dislash/slash_commands/slash_command.py
+++ dislash/slash_commands/slash_command.py
@@ -28,16 +28,16 @@ class Type:
     MENTIONABLE = 9
     NUMBER = 10
     """
-    SUB_COMMAND       = 1
+    SUB_COMMAND = 1
     SUB_COMMAND_GROUP = 2
-    STRING            = 3
-    INTEGER           = 4
-    BOOLEAN           = 5
-    USER              = 6
-    CHANNEL           = 7
-    ROLE              = 8
-    MENTIONABLE       = 9
-    NUMBER            = 10
+    STRING = 3
+    INTEGER = 4
+    BOOLEAN = 5
+    USER = 6
+    CHANNEL = 7
+    ROLE = 8
+    MENTIONABLE = 9
+    NUMBER = 10
 
 
 class OptionChoice:
